9341bd7c0982cd0b61fd68d328b12b42bee1d04e
akshay-pise <akshaybspise@gmail.com>
2022-09-09 23:14:41 +0530

    variable and data type

---
+++ day2/js/js.js
@@ -0,0 +1,18 @@
+var name="akshay";// var name='akshay';
+var phoneNumber=1234567890;
+var learningJs=true;
+var age;
+var time=null;
+var learningLanguage={
+    languageCounr:1,
+    LanguageName:'html'
+}
+var ratingPoint=[7,9,5];
+
+console.log("name datatype - "+typeof(name));
+console.log("phoneNumber datatype - "+typeof(phoneNumber));
+console.log("learningJsBoolean datatype - "+typeof(learningJs));
+console.log("ageUndefined datatype - "+typeof(age));
+console.log("timeNull datatype - "+typeof(time));
+console.log("learningLanguageObject datatype - "+typeof(learningLanguage));
+console.log("ratingPointArray datatype - "+typeof(ratingPoint));
